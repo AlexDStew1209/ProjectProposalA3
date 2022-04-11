4c78180782dc54d5c084690574a94e6ef264b608
AlexDStew1209 <101136139+AlexDStew1209@users.noreply.github.com>
2022-04-11 12:29:20 -0600

    Update Player.py

--- src/Player.py
+++ src/Player.py
@@ -1,1 +1,18 @@
-class Player
+class Player:
+    #Member Variables
+    x = 0
+    y = 0
+    health = 100
+    lives = 3
+    pColor = 0
+    #Constructor
+    def __init__(self,x,y,pColor):
+        self.x = x
+        self.y = y
+        self.pColor = pColor
+    #Methods
+    def display(self):
+        fill(self.pColor)
+        rect(x,y,25,50)
+        
+
