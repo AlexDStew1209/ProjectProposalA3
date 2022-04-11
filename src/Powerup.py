312c7ada2d34927cddfdb976cbf6208b9adda29e
Tanuk12 <101136696+Tanuk12@users.noreply.github.com>
2022-04-11 12:29:07 -0600

    Update Powerup.py

--- src/Powerup.py
+++ src/Powerup.py
@@ -1,1 +1,18 @@
-class Powerup
+class Powerup:
+    l = 0
+    w = 0
+    h = 0
+    x = 0
+    y = 0
+    def__init__(self):
+        fill(230)
+        self.l = 0
+        self.w = 0
+        self.h = 0
+        self.x = 0
+        self.y = 0
+    #methods 
+    def display(self):
+    def attack(self):
+    def health(self):
+    def defence(self):
