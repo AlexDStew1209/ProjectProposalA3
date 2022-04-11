353ee2a968cb6a59fa8f0446139255ef5fdd6ba1
AlexDStew1209 <101136139+AlexDStew1209@users.noreply.github.com>
2022-04-11 12:31:15 -0600

    Update Arm.py

--- src/Arm.py
+++ src/Arm.py
@@ -1,1 +1,14 @@
-class Arm
+class Arm:
+    #Member Variables
+    reach = 10
+    armColor = 0
+    damage = 10
+    #Constructor
+    def __init__(self,armColor):
+        
+        self.armColor = armColor
+    #Methods
+    def display(self):
+        fill(self.armColor)
+        rect(x,y,25,50)
+        
