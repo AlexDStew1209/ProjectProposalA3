1ce983c9d296c2a652da1271ff40dfc6122354f0
AlexDStew1209 <101136139+AlexDStew1209@users.noreply.github.com>
2022-04-13 11:57:52 -0600

    Update Startscreen.py

--- src/Startscreen.py
+++ src/Startscreen.py
@@ -1,1 +1,21 @@
 class Startscreen:
+    #Member Variables
+    screen = 0
+    
+    #Constructor
+    def __init__(self):
+        self.screen = 0
+        
+    #Methods
+    def display(self):
+        startScreen()
+        
+        
+    def startScreen(self):
+        if screen == 0:
+            background(40)
+            textSize(72)
+            textAlign(CENTER)
+            text("Welcome to Space Assailant", 750,500)
+        
+        
