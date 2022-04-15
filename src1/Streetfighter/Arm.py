b29a4bdb7bb4861f21c476355373d3d65284970d
stuartpahnkake <89059554+stuartpahnkake@users.noreply.github.com>
2022-04-15 12:24:06 -0600

    Add files via upload

---
+++ src1/Streetfighter/Arm.py
@@ -0,0 +1,13 @@
+class Arm:
+    #Member Variables
+    reach = 10
+    armColor = 0
+    damage = 10
+    #Constructor
+    def __init__(self,armColor):
+        
+        self.armColor = armColor
+    #Methods
+    def display(self):
+        fill(self.armColor)
+        rect(x,y,25,50)
