3d0214091be3c3a0d02373e930eb1e10d0c2d847
AlexDStew1209 <101136139+AlexDStew1209@users.noreply.github.com>
2022-04-25 12:19:44 -0600

    Update Player.py

--- src1/Streetfighter/Player.py
+++ src1/Streetfighter/Player.py
@@ -1,16 +1,23 @@
+#By Alex Stewart
+
 class Player:
     #Member Variables
     x = 0
     y = 0
     health = 100
     lives = 3
-    pColor = 0
+    pColor1 = 0
+    pColor2 = 0
+    pColor3 = 0
     #Constructor
-    def __init__(self,x,y,pColor):
+    def __init__(self,x,y,pColor1,pColor2,pColor3):
         self.x = x
         self.y = y
-        self.pColor = pColor
+        self.pColor2 = pColor2
+        self.pColor1 = pColor1
+        self.pColor3 = pColor3
     #Methods
     def display(self):
-        fill(self.pColor)
-        rect(x,y,25,50)
+        fill(self.pColor1,self.pColor2,self.pColor3)
+        rect(self.x,self.y,100,200)
+        
