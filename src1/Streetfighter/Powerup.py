0a266dea15171efed7a6b3706de93682de36b412
Tanuk12 <101136696+Tanuk12@users.noreply.github.com>
2022-05-04 11:43:51 -0600

    Update Powerup.py

--- src1/Streetfighter/Powerup.py
+++ src1/Streetfighter/Powerup.py
@@ -1,14 +1,18 @@
 class Powerup:
-    l = 0
-    w = 0
-    h = 0
-    x = 0
-    y = 0
+    size(100, 100)
+    photo = loadImage("defence.png")
+    image(photo, 0, 0)size(100, 100)
+    photo = loadImage("attack.png")
+    image(photo, 0, 0)size(100, 100)
+    photo = loadImage("health.png")
+    image(photo, 0, 0)
+    x = random
+    y = random
     def__init__(self):
         fill(230)
         self.l = 0
         self.w = 0
-        self.h = 0
+        self.h = 0 
         self.x = 0
         self.y = 0
     #methods 
