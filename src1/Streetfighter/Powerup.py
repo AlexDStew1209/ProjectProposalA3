9a3339ddb8262c4cfe1cd53374a34c98f5d8b86d
AlexDStew1209 <101136139+AlexDStew1209@users.noreply.github.com>
2022-05-20 12:49:37 -0600

    Update Powerup.py

--- src1/Streetfighter/Powerup.py
+++ src1/Streetfighter/Powerup.py
@@ -1,20 +1,24 @@
 class Powerup:
     powerUpType = 0
-    x = random(150,1350)
-    y = random(150,700)
+    x = random(50,450)
+    y = random(50,450)
+    powerUpType = int(random(3))
     def __init__(self):
-        self.x = random(150,1350)
-        self.y = random(150,700)
+        self.x = 50
+        self.y = 50
+        self.powerUpType = int(random(3))
 
     #methods 
     def display(self):
-        powerUpType = random(3)
-        if powerUpType == 0:
+        
+        if self.powerUpType == 0:
             defence = loadImage("defence.png")
             image(defence, self.x, self.y)
-        elif powerUpType == 1:
+        elif self.powerUpType == 1:
             attack = loadImage("attack.png")
             image(attack, self.x, self.y)
-        elif powerUpType == 2:
+        elif self.powerUpType == 2:
             health = loadImage("health.png")
             image(health, self.x, self.y)
+        
+        
